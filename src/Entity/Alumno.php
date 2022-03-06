49f68d70170efb578f72cd80680a92cd10af3625
junxian428 <holeyiho@gmail.com>
2022-03-06 15:04:21 +0800

    symfony learning

---
+++ src/Entity/Alumno.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlumnoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AlumnoRepository::class)]
+class Alumno
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50, nullable: true)]
+    private $nombre;
+
+    #[ORM\Column(type: 'string', length: 50, nullable: true)]
+    private $apellidop;
+
+    #[ORM\Column(type: 'string', length: 50, nullable: true)]
+    private $apellidos;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $edad;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(?string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidop(): ?string
+    {
+        return $this->apellidop;
+    }
+
+    public function setApellidop(?string $apellidop): self
+    {
+        $this->apellidop = $apellidop;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(?string $apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getEdad(): ?int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(?int $edad): self
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+}
---
+++ src/Form/AlumnoType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Alumno;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AlumnoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nombre')
+            ->add('apellidop')
+            ->add('apellidos')
+            ->add('edad')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Alumno::class,
+        ]);
+    }
+}
